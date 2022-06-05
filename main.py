fe2f75ca654ec87d905cb7b1ee71103d91f89d73
Hoanh An <hoanhan@veryfi.com>
2022-06-04 21:29:39 -0700

    Checkin TimeSheetX

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ jtemplates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return jtemplates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/projects/{name}", response_class=HTMLResponse)
+def projects(request: Request, name: str):
+    pretty_name = name
+    if name == "timesheetx":
+        pretty_name = "TimeSheetX"
+
+    return jtemplates.TemplateResponse(
+        f"projects_{name}.html", {"request": request, "name": pretty_name}
+    )
