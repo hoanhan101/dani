05b75188e83767d1f9e95a6a47b27fd32222d861
Hoanh An <hoanhan@veryfi.com>
2022-06-11 00:32:22 -0700

    Checkin current progress

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def index(request: Request):
     return jtemplates.TemplateResponse("index.html", {"request": request})
 
 
+@app.get("/about", response_class=HTMLResponse)
+def index(request: Request):
+    return jtemplates.TemplateResponse("about.html", {"request": request})
+
+
+@app.get("/otherside", response_class=HTMLResponse)
+def index(request: Request):
+    return jtemplates.TemplateResponse("otherside.html", {"request": request})
+
+
 @app.get("/projects/{name}", response_class=HTMLResponse)
 def projects(request: Request, name: str):
     return jtemplates.TemplateResponse(f"projects_{name}.html", {"request": request})
